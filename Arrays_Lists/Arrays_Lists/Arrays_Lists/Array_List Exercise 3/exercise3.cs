fb65ab62ea45339206dec15e3cf84313b779ceb0
NovaCrusade <79559369+NovaCrusade@users.noreply.github.com>
2023-11-19 15:23:04 +0000

    Functionality of arrays and lists

---
+++ Arrays_Lists/Arrays_Lists/Arrays_Lists/Array_List Exercise 3/exercise3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_Lists.Array_List_Exercise_3
+{
+    internal class exercise3
+    {
+        static void Main(string[] args)
+        {
+             var numbers = new int?[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Please enter number {i + 1}.");
+
+                do
+                {
+                    int n;
+                    while (!int.TryParse(Console.ReadLine(), out n))
+                        Console.WriteLine("Invalid number. Please try again.");
+
+                    var currentNumber = Convert.ToInt32(n);
+                    var containsNumber = numbers.Contains(currentNumber);
+
+                    if (!containsNumber)
+                    {
+                        numbers[i] = currentNumber;
+                        break;
+                    }
+
+                    Console.WriteLine("Number was entered previously, please enter a different number.");
+                } while (true);
+
+
+            }
+
+            Console.Clear();
+            Array.Sort(numbers);
+
+            foreach (int? n in numbers)
+                Console.WriteLine(n);
+
+            Console.ReadLine();
+        }
+    }
+}
