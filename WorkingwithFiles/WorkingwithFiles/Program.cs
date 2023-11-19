8e94029bc1c95de3a3509f96e6dcd5d3151ed3f8
NovaCrusade <79559369+NovaCrusade@users.noreply.github.com>
2023-11-19 15:19:21 +0000

    learning to handle Dir & files with code

---
+++ WorkingwithFiles/WorkingwithFiles/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace CSharpfund
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\somefile.jjpg";
+
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\mufile.jpg", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //
+            }
+
+            var context = File.ReadAllText(path);
+
+            var fileinfo = new FileInfo(path);
+            FileInfo.CopyTo("...");
+            FileInfo.Delete();
+            if (fileinfo.Exists)
+            {
+                //
+            }
+
+
+        }
+
+    }
+}
---
+++ WorkingwithText/WorkingwithText/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Csharpfund
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            var fullName = "Man dem";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            Console.WriteLine("ToUpper: '{0}'", fullName.ToUpper());
+            Console.WriteLine("ToUpper: '{0}'", fullName.ToUpper());
+
+            var Index = fullName.IndexOf(' ');
+            var firstname = fullName.Substring(0, Index);
+            var lastname = fullName.Substring(Index + 1);
+            Console.WriteLine("Firstname: " + firstname);
+            Console.WriteLine("Lastname: " + lastname);
+
+            var names = fullName.Split(' ');
+            Console.WriteLine("firstname: " + names[0]);
+            Console.WriteLine("lastname: " + names[1]);
+
+            Console.WriteLine(fullName.Replace("Mosh", "Pittino"));
+
+            var str = "25";
+            var age = Convert.ToByte(str);
+            Console.WriteLine(age);
+
+            float price = 29.95f;
+            Console.WriteLine(price.ToString("C0"));
+        }
+    }
+}
