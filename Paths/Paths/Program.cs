0f855163ddb1fec4b740e026c164ef08d459ee74
NovaCrusade <79559369+NovaCrusade@users.noreply.github.com>
2023-11-22 20:26:37 +0000

    using C# to access paths

---
+++ Paths/Paths/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace CSharpfund
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"D:\Portfolio\NovaWorks\Self-Taught\C#\C-Learning\StringBuilder\StringBuilder.sln";
+
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File Name: " + Path.GetFileName(path));
+            Console.WriteLine("File Name without Extesion: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory Name: " + Path.GetDirectoryName(path));
+        }
+    }
+}
